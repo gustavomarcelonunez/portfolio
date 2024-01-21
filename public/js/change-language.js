100e897ee75f5b05048f23c44e9af735a3715c66
Gustavo Marcelo Nuñez <121830867+gustavomarcelonunez@users.noreply.github.com>
2024-01-20 22:03:57 -0300

    Add files via upload

---
+++ public/js/change-language.js
@@ -0,0 +1,18 @@
+function changeLanguage(language) {
+    console.log('Cambiando a idioma:', language);
+    fetch(`../../portfolio/public/${language}.json`)
+    .then(response => response.json())
+    .then(data => updateContent(data))
+    .catch(error => console.error('Error al cargar el archivo JSON', error));
+}
+
+function updateContent(data) {
+    Object.keys(data).forEach(key => {
+        const element = document.querySelector(`[data-lang="${key}"]`);
+        if (element) {
+            element.innerHTML = data[key];
+        }
+    });
+}
+
+changeLanguage('es');
